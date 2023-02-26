04f386f603f54fc748775b900f14f1c00896f130
Manish Bishnoi <74095019+kodedriller@users.noreply.github.com>
2023-02-26 10:37:04 +0530

    Q.344

---
+++ reverse-string.cpp
@@ -0,0 +1,18 @@
+// class Solution{
+//     public:
+//     void reverseString(vector<char>& s){
+//         int l=0,r=s.size()-1;
+//         while(l<r){
+//             swap(s[l],s[r]);
+//             l++;
+//             r--;
+//         }
+//     }
+// };
+
+class Solution{
+    public:
+    void reverseString(vector<char>& s){
+        reverse(s.begin(), s.end());
+    }
+};
