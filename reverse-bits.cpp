f4a20dce9f524642b83f2808cf32132e6f129843
Manish Bishnoi <74095019+kodedriller@users.noreply.github.com>
2023-02-28 14:33:23 +0530

    Q.190

---
+++ reverse-bits.cpp
@@ -0,0 +1,15 @@
+// 190. Reverse Bits
+
+class Solution{
+    public:
+    uint32_t reverseBits(uint32_t n){
+        uint32_t reversed =0;
+        for(int i=0;i<32;i++){
+            reversed <<=1;
+            reversed |= n&1;
+            n>>=1;
+
+        }
+        return reversed;
+    }
+};
