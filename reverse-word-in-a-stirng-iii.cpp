6e71693ea3ac59eabc2e21bd5919a85822118619
Manish Bishnoi <74095019+kodedriller@users.noreply.github.com>
2023-02-28 14:39:07 +0530

    Q.557

---
+++ reverse-word-in-a-stirng-iii.cpp
@@ -0,0 +1,16 @@
+// 557. Reverse Words in a String III
+
+class Solution{
+    public:
+    string reverseWords(string s){
+        int start =0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                reverse(s.begin()+start, s.begin()+i);
+                start=i+1;
+            }
+        }
+        reverse(s.begin+start, s.end());
+            return s;
+    }
+};
